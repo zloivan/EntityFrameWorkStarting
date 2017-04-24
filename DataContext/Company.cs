3ff3e99342d6173d8869458b849e61abe97546a3
Иван Хомичков <ivan.khomichkov@gmail.com>
2017-04-24 20:52:11 +0300

    Created new folder in project where new projexts based on linq will be stored. Addaed dbContext to this project. 2 models (Phones and Companies).

---
+++ DataContext/Company.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace DataContext
+{
+    public class Company
+    {
+        public Company()
+        {
+            Phones = new List<Phone>();
+        }
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public List<Phone> Phones { get; set; }
+    }
+} 
---
+++ DataContext/DataModel.cs
@@ -0,0 +1,37 @@
+﻿namespace DataContext
+{
+    using System;
+    using System.Data.Entity;
+    using System.Linq;
+
+    public class DataModel : DbContext
+    {
+        // Контекст настроен для использования строки подключения "DataModel" из файла конфигурации  
+        // приложения (App.config или Web.config). По умолчанию эта строка подключения указывает на базу данных 
+        // "DataContext.DataModel" в экземпляре LocalDb. 
+        // 
+        // Если требуется выбрать другую базу данных или поставщик базы данных, измените строку подключения "DataModel" 
+        // в файле конфигурации приложения.
+        static DataModel()
+        {
+            Database.SetInitializer(new MyContextInitializer());
+        }
+        public DataModel()
+            : base("name=DataModel")
+        {
+        }
+
+        public virtual DbSet<Company> Companies { get; set; }
+
+        // Добавьте DbSet для каждого типа сущности, который требуется включить в модель. Дополнительные сведения 
+        // о настройке и использовании модели Code First см. в статье http://go.microsoft.com/fwlink/?LinkId=390109.
+
+         public virtual DbSet<Phone> Phones { get; set; }
+    }
+
+    //public class MyEntity
+    //{
+    //    public int Id { get; set; }
+    //    public string Name { get; set; }
+    //}
+}
