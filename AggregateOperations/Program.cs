4e5c77b5cf99714e2fb5910453f7699df7d4daec
Иван Хомичков <ivan.khomichkov@gmail.com>
2017-04-27 00:48:02 +0300

    Created base for Agregate operations...

---
+++ AggregateOperations/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataContext;
+namespace AggregateOperations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (DataModel db = new DataModel())
+            {
+
+                 
+            }
+        }
+    }
+}
